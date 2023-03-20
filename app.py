549542d78fdda85e20ccc9775af134cfeb1bd6de
praveen pandey <114177903+praveenpandey77@users.noreply.github.com>
2023-03-20 09:58:29 +0000

    added image and some advance steps

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = [
-  {'NAME': 'PRAVEEN', 
-   'CLASS': 'JOVIAN', 
-   'SUBJECT': 'PYTHON'}
+JOBS=[
+  {
+    'NAME': 'PRAVEEN',
+    'ADDRESS': 'ASHOK NAGAR',
+    'SALARY': '10,0000',
+    'STREET SYMBOL': 'DELTA'
+  },
+  {
+    'NAME': 'namrata',
+    'ADDRESS': 'parpodi',
+    'SALARY': '15,0000',
+    'STREET SYMBOL' : 'sigma'
+  }
 ]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', details=JOBS, character='life details in sanskrit')
 
 
 if __name__ == "__main__":
